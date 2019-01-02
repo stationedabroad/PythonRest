3a89de063dcadf571efac7f0b634a4c47839edba
Sulman <sulmanmh@gmail.com>
2019-01-02 09:17:06 +0000

    Initial commit from X220

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/stationedabroad/Documents/pyWeb/virtual-envs/djangoRest/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
